2cded57177a62c44c9033267d59fab2e15110ca5
EmptyCup <jaydenha000@gmail.com>
2026-02-05 23:20:12 +0900

    Added play function

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-import mido
-import time
+import mido, pyautogui, time, json
 
 def main():
     tst = 'TestMiddleC.mid'
     mid = mido.MidiFile(tst)
 
-    mapping = {
-        "48" : 'C4',
-        "52" : 'E4',
-        "55" : 'G4',
-        "60" : 'C5'
-    }
+    with open('mapping.json', 'r') as file:
+        data = file.read()
+
+    mapping = json.loads(data)
 
     sheetMusic = makeSheet(mid)
-
-    for note in sheetMusic:
-        time.sleep(note.time)
-        print(note.type)
+    play(sheetMusic, mapping)
     
 
 def makeSheet(mid):
     class Note:
-        def __init__(self, type, note, time):
+        def __init__(self, type, note_no, time):
             self.type = type # 'note_on' or 'note_off'
-            self.note = note # 'the number value for the note'
+            self.note_no = note_no # 'the number value for the note'
             self.time = time
 
     sheetMusic = []
 
     for msg in mid:
         if not msg.is_meta:
-            sheetMusic.append(Note(msg.type, msg.note, msg.time))
+            sheetMusic.append(Note(msg.type, str(msg.note), msg.time)) # msg.note into string as json file only takes srings as keys
     
     return(sheetMusic)
 
+def play(sheetMusic, mapping):
+
+    for note in sheetMusic:
+        time.sleep(note.time)
+
+        if note.type == 'note_on':
+            pyautogui.keyDown(mapping[note.note_no])
+        if note.type == 'note_off':
+            pyautogui.keyUp(mapping[note.note_no])
 
 main()
