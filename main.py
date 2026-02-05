f6a36e09de8e3dff825609d072bdb4a32675a481
EmptyCup <jaydenha000@gmail.com>
2026-02-05 22:14:30 +0900

    Made makeSheet function

--- main.py
+++ main.py
@@ -5,7 +5,34 @@ def main():
     tst = 'TestMiddleC.mid'
     mid = mido.MidiFile(tst)
 
+    mapping = {
+        "48" : 'C4',
+        "52" : 'E4',
+        "55" : 'G4',
+        "60" : 'C5'
+    }
+
+    sheetMusic = makeSheet(mid)
+
+    for note in sheetMusic:
+        time.sleep(note.time)
+        print(note.type)
+    
+
+def makeSheet(mid):
+    class Note:
+        def __init__(self, type, note, time):
+            self.type = type # 'note_on' or 'note_off'
+            self.note = note # 'the number value for the note'
+            self.time = time
+
+    sheetMusic = []
+
     for msg in mid:
-        time.sleep(msg.time)
         if not msg.is_meta:
-            print(msg)
+            sheetMusic.append(Note(msg.type, msg.note, msg.time))
+    
+    return(sheetMusic)
+
+
+main()
